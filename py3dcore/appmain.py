08f10c4edb5c65a8d39ec8dabe9a24f9062d0cfd
Hannah Theresa Ruedisser <hannahruedisser@Hannahs-Laptop.local>
2023-05-10 10:26:57 +0200

    include h4c updates

---
+++ py3dcore/appmain.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+def run():
+    
+    # set page config
+    st.set_page_config(page_title='py3DCORE', page_icon = ':satellite:', # :rocket:, :sun:, :ringed_planet:, :star:, :telescope:, 
+                       initial_sidebar_state='expanded') 
+
+
+
+
+if __name__ == '__main__':
+    run()
