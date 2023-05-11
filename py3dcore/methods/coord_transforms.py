1405614d84840692cdc9dbf9408060fcd46ca1ef
Hannah Theresa Ruedisser <hannahruedisser@Hannahs-Laptop.local>
2023-05-11 11:46:25 +0200

    add preprocess

--- py3dcore/methods/coord_transforms.py
+++ py3dcore/methods/coord_transforms.py
@@ -1,41 +1,7 @@
 import numpy as np
 import pandas as pd
-from datetime import datetime, timedelta
+import datetime
 import itertools
-
-def transform_reference_frame(
-    dtp: Union[dt.datetime, Sequence[dt.datetime]],
-    vec_array: np.ndarray,
-    reference_frame_from: str,
-    reference_frame_to: str,
-) -> np.ndarray:
-    if reference_frame_from == reference_frame_to:
-        return vec_array
-
-    if isinstance(dtp, dt.datetime):
-        dtp = [dtp] * len(vec_array)
-
-    # convert to datetime objects
-    if not isinstance(dtp[0], dt.datetime):
-        dtp = [dt.datetime.fromtimestamp(_t, dt.timezone.utc) for _t in dtp]
-
-    vec_array_new = np.zeros_like(vec_array)
-
-    if vec_array.ndim == 1:
-        vec_array.reshape(-1, len(vec_array))
-
-    if vec_array.ndim == 2:
-        for i in range(0, len(dtp)):
-            vec_array_new[i] = spiceypy.mxv(
-                spiceypy.pxform(
-                    reference_frame_from,
-                    reference_frame_to,
-                    spiceypy.datetime2et(dtp[i]),
-                ),
-                vec_array[i],
-            )
-
-    return vec_array_new
 
 
 #input datetime to return T1, T2 and T3 based on Hapgood 1992
